b904c9a97c61527eac3f3e091c1e5460b181d991
sele906 <seunga906@gmail.com>
2026-09-01 17:27:52 +0900

    add api url

--- web/src/api/libraryApi.js
+++ web/src/api/libraryApi.js
@@ -1,6 +1,7 @@
+const API_URL = import.meta.env.VITE_API_URL || ''
 
 export async function getConnection() {
-    const response = await fetch('/api/connection');
+    const response = await fetch(`${API_URL}/api/connection`)
 
     console.log(response);
 
